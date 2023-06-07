d9ba29a825943fcccc4aac8db7d21124cfa3188b
Brian Wilson <brian@wildsong.biz>
2023-06-07 08:34:06 -0700

    run_ script tuning

--- run_certbot.sh
+++ run_certbot.sh
@@ -12,7 +12,7 @@ docker run --rm -v $PWD/certs:/etc/letsencrypt:rw cc/certbot certonly \
        --max-log-backups=0 \
        --allow-subset-of-names \
        --dns-dnsmadeeasy --dns-dnsmadeeasy-credentials /usr/local/lib/dnsmadeeasy.ini \
-       --quiet --noninteractive
+       --quiet --noninteractive 2>/tmp/certbot_noise
 
 # Update hitch
 # I should only do this if the hitch bundle changed.
@@ -21,6 +21,8 @@ age=$(stat -c %Y $hitch)
 now=$(date +"%s")
 if (( ($now - $age) < (60 * 60) )); then
     echo $hitch changed
-    docker stack rm varnish 
+    docker stack rm varnish
+    # give varnish_network time to disappear
+    sleep 5
     docker stack deploy --with-registry-auth -c compose.yaml varnish 
 fi
--- run_cloudflare_certbot.sh
+++ run_cloudflare_certbot.sh
@@ -21,7 +21,7 @@ docker run --rm -v $PWD/certs:/etc/letsencrypt:rw cc/certbot \
        --disable-hook-validation \
        --max-log-backups=0 \
        --dns-cloudflare --dns-cloudflare-credentials /usr/local/lib/cloudflare.ini \
-       --quiet --noninteractive
+       --quiet --noninteractive 2>/tmp/certbot_noise
 
 # Update hitch
 hitch="certs/hitch-bundle.pem"
@@ -29,6 +29,8 @@ age=$(stat -c %Y $hitch)
 now=$(date +"%s")
 if (( ($now - $age) < (60 * 60) )); then
     echo $hitch changed
-    docker stack rm varnish 
+    docker stack rm varnish
+    # give varnish_network time to disappear
+    sleep 5
     docker stack deploy --with-registry-auth -c compose.yaml varnish 
 fi
